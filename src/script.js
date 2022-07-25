8d916ba070ff1ca92441354fc32d8c08f32af88e
msynk <msynk@outlook.com>
2022-07-25 16:07:27 +0430

    add swipe to slide feature

--- src/script.js
+++ src/script.js
@@ -1,41 +1,77 @@
+let isDown = false;
+let x = -1;
+const carousel = document.querySelector('.crsl');
+carousel.addEventListener('pointerdown', e => {
+  isDown = true;
+  x = e.clientX;
+  carousel.style.cursor = 'grabbing';
+});
+carousel.addEventListener('pointerup', resetPointer);
+carousel.addEventListener('pointerleave', resetPointer);
+function resetPointer() {
+  isDown = false;
+  carousel.style.cursor = 'grab';
+}
+
+carousel.addEventListener('pointermove', e => {
+  if (!isDown) return;
+
+  e.preventDefault();
+
+  const delta = e.clientX - x;
+
+  if (!isInTransition && Math.abs(delta) > 20) {
+    isDown = false;
+    carousel.style.cursor = 'grab';
+    delta < 0 ? next(true) : prev(true);
+  }
+});
+
+carousel.addEventListener('touchmove', e => {
+  e.preventDefault();
+})
+
+
 let currentIndex = 1;
-let nextIndex = 1;
+let otherIndex = 1;
 let isInTransition = false;
 
-function prev() {
+function prev(delayed) {
   if (isInTransition) return;
 
-  nextIndex = currentIndex - 1;
-  if (nextIndex < 1) nextIndex = 4;
+  otherIndex = currentIndex - 1;
+  if (otherIndex < 1) otherIndex = 4;
 
-  go();
+  go(delayed);
 }
 
-function next() {
+function next(delayed) {
   if (isInTransition) return;
 
-  nextIndex = currentIndex + 1;
-  if (nextIndex > 4) nextIndex = 1;
+  otherIndex = currentIndex + 1;
+  if (otherIndex > 4) otherIndex = 1;
 
-  go(true);
+  go(delayed, true);
 }
 
-function go(isNext) {
+function go(delayed, isNext) {
   isInTransition = true;
 
   const current = document.querySelector(
     `.crsl .item:nth-child(${currentIndex})`
   );
   const other = document.querySelector(
-    `.crsl .item:nth-child(${nextIndex})`
+    `.crsl .item:nth-child(${otherIndex})`
   );
 
-  other.classList.add(isNext ? "right" : "left", "active", "entering");
+  other.classList.add(isNext ? "right" : "left", "active", "entering", "transition");
   current.classList.add(isNext ? "left" : "right", "transition");
 
+  // to prevent the weird behavior of the chromium browsers when the devtools is open, use this 20 instead
+  const delayedTimeout = 0; // 20;
   setTimeout(() => {
-    other.classList.add("transition", "center");
-  });
+    other.classList.add("center");
+  }, delayed ? delayedTimeout : 0);
 
   setTimeout(() => {
     current.classList.remove("left", "right", "active", "transition");
@@ -43,5 +79,5 @@ function go(isNext) {
     isInTransition = false;
   }, 500);
 
-  currentIndex = nextIndex;
+  currentIndex = otherIndex;
 }
