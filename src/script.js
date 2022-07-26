60f4b67f8f665a6cc286eb952b49f223e2e8ac4c
msynk <msynk@outlook.com>
2022-07-26 22:47:01 +0430

    first impl. of advanced slider

--- src/script.js
+++ src/script.js
@@ -1,6 +1,100 @@
-let isDown = false;
 let x = -1;
+let isDown = false;
 const carousel = document.querySelector('.crsl');
+const items = [].slice.call(carousel.children);
+const itemsCount = items.length;
+
+const showCount = 4;
+const scrollCount = 2;
+
+const transitionTime = 0.5;
+
+let currentIndices = [];
+let otherIndices = [];
+let isInTransition = false;
+
+currentIndices = Array(showCount).fill().map((_, i) => i);
+otherIndices = Array(scrollCount).fill().map((_, i) => i);
+
+items.forEach(it => carousel.removeChild(it));
+currentIndices.forEach(i => carousel.appendChild(items[i]));
+
+function prev(delayed) {
+  if (isInTransition) return;
+
+  otherIndices = otherIndices.map((_, i) => {
+    let idx = currentIndices[0] - (i + 1);
+    if (idx < 0) idx = itemsCount + idx;
+    return idx;
+  }).reverse();
+
+  go(delayed);
+}
+
+function next(delayed) {
+  if (isInTransition) return;
+
+  otherIndices = otherIndices.map((_, i) => {
+    let idx = currentIndices[showCount - 1] + (i + 1);
+    if (idx > itemsCount - 1) idx = idx - itemsCount;
+    return idx;
+  });
+
+  go(delayed, true);
+}
+
+function go(delayed, isNext) {
+  isInTransition = true;
+  const newIndices = isNext
+    ? [...currentIndices.slice(scrollCount - currentIndices.length), ...otherIndices]
+    : [...otherIndices, ...currentIndices.slice(0, currentIndices.length - scrollCount)];
+
+  const currents = currentIndices.map(i => items[i]);
+  const others = otherIndices.map(i => items[i]);
+  const news = newIndices.map(i => items[i]);
+
+  const { width, height } = currents[0].getBoundingClientRect();
+  carousel.style.height = `${height}px`;
+  let sign = isNext ? 1 : -1;
+  let offset = isNext ? showCount : scrollCount;
+  others.forEach((o, i) => {
+    o.style.width = `${width}px`;
+    o.style.transform = `translateX(${sign * 100 * (offset + (sign * i))}%)`;
+    o.style.transition = `all ${transitionTime}s`;
+    o.classList.add("absolute")
+  });
+  others.forEach(o => carousel.appendChild(o));
+
+  sign *= -1;
+  currents.forEach((c, i) => {
+    c.style.width = `${width}px`;
+    c.style.transform = `translateX(${100 * i}%)`;
+    c.classList.add("absolute");
+  });
+
+  // to prevent the weird behavior of the chromium browsers when the devtools is open, use this 20 instead
+  const delayedTimeout = 0; // 20;
+  offset = isNext ? showCount - scrollCount : 0;
+  setTimeout(() => {
+    others.forEach((o, i) => o.style.transform = `translateX(${100 * (offset + i)}%)`);
+    currents.forEach((c, i) => (c.style.transition = `all ${transitionTime}s`, c.style.transform = `translateX(${sign * 100 * (scrollCount + (sign * i))}%)`));
+  }, delayed ? delayedTimeout : 0);
+
+  setTimeout(() => {
+    currents.forEach(c => (c.style.transform = '', c.style.transition = '', c.classList.remove("absolute"), carousel.removeChild(c)));
+    news.forEach(n => (n.style.width = '', n.style.transform = '', n.style.transition = '', n.classList.remove("absolute"), carousel.appendChild(n)));
+    carousel.style.height = '';
+
+    isInTransition = false;
+  }, transitionTime * 1000);
+
+  currentIndices = newIndices;
+}
+
+
+
+
+
 carousel.addEventListener('pointerdown', e => {
   isDown = true;
   x = e.clientX;
@@ -10,9 +104,8 @@ carousel.addEventListener('pointerup', resetPointer);
 carousel.addEventListener('pointerleave', resetPointer);
 function resetPointer() {
   isDown = false;
-  carousel.style.cursor = 'grab';
+  carousel.style.cursor = '';
 }
-
 carousel.addEventListener('pointermove', e => {
   if (!isDown) return;
 
@@ -22,62 +115,10 @@ carousel.addEventListener('pointermove', e => {
 
   if (!isInTransition && Math.abs(delta) > 20) {
     isDown = false;
-    carousel.style.cursor = 'grab';
+    carousel.style.cursor = '';
     delta < 0 ? next(true) : prev(true);
   }
 });
-
 carousel.addEventListener('touchmove', e => {
   e.preventDefault();
 })
-
-
-let currentIndex = 1;
-let otherIndex = 1;
-let isInTransition = false;
-
-function prev(delayed) {
-  if (isInTransition) return;
-
-  otherIndex = currentIndex - 1;
-  if (otherIndex < 1) otherIndex = 4;
-
-  go(delayed);
-}
-
-function next(delayed) {
-  if (isInTransition) return;
-
-  otherIndex = currentIndex + 1;
-  if (otherIndex > 4) otherIndex = 1;
-
-  go(delayed, true);
-}
-
-function go(delayed, isNext) {
-  isInTransition = true;
-
-  const current = document.querySelector(
-    `.crsl .item:nth-child(${currentIndex})`
-  );
-  const other = document.querySelector(
-    `.crsl .item:nth-child(${otherIndex})`
-  );
-
-  other.classList.add(isNext ? "right" : "left", "active", "entering", "transition");
-  current.classList.add(isNext ? "left" : "right", "transition");
-
-  // to prevent the weird behavior of the chromium browsers when the devtools is open, use this 20 instead
-  const delayedTimeout = 0; // 20;
-  setTimeout(() => {
-    other.classList.add("center");
-  }, delayed ? delayedTimeout : 0);
-
-  setTimeout(() => {
-    current.classList.remove("left", "right", "active", "transition");
-    other.classList.remove("left", "right", "center", "transition", "entering");
-    isInTransition = false;
-  }, 500);
-
-  currentIndex = otherIndex;
-}
